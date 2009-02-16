3f926bd86295db65d373239296f23a491d7a3b46
benwarfield <benwarfield@ff732b92-f563-11dd-9d1d-173cc1d06e3c>
2009-02-16 19:28:38 +0000

    Moved syscall_fail.c, and updated entry.S.

--- trunk/arch/i386/kernel/syscall_fail.c
+++ trunk/arch/i386/kernel/syscall_fail.c
@@ -1,9 +1,5 @@
 /* fail.c: implementation of fail syscall */
 
-
-#include <fail.h>
-
-
 _syscall3(int, fail, int, ith,
 	 int, ncall, 
 	 struct syscall_failure *, 
@@ -11,10 +7,4 @@ _syscall3(int, fail, int, ith,
 	 
 int sys_fail(int ith, int ncall, struct syscall_failure *calls) {
 
-}
-
-
-void free_fail(struct task_struct *tsk)
-{
-  if (NULL != tsk->fail_vector) kfree(tsk->fail_vector);
 }
