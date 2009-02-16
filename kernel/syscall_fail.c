dbd0b0b43c9790ac098052a7a4f536cc32fe5502
thierrybm@hotmail.com <thierrybm@hotmail.com@ff732b92-f563-11dd-9d1d-173cc1d06e3c>
2009-02-16 19:15:29 +0000

    free_fail now in fail.h

--- kernel/syscall_fail.c
+++ kernel/syscall_fail.c
@@ -1,5 +1,9 @@
 /* fail.c: implementation of fail syscall */
 
+
+#include <fail.h>
+
+
 _syscall3(int, fail, int, ith,
 	 int, ncall, 
 	 struct syscall_failure *, 
@@ -7,4 +11,10 @@ _syscall3(int, fail, int, ith,
 	 
 int sys_fail(int ith, int ncall, struct syscall_failure *calls) {
 
+}
+
+
+void free_fail(struct task_struct *tsk)
+{
+  if (NULL != tsk->fail_vector) kfree(tsk->fail_vector);
 }
