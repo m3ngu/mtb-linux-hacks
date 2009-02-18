09ea7d063794d3bffa6d1876e585a746ffeddfd1
msukan <msukan@ff732b92-f563-11dd-9d1d-173cc1d06e3c>
2009-02-18 18:26:59 +0000

    Updated test_fail.c to comply with specification

--- trunk/test_fail.c
+++ trunk/test_fail.c
@@ -4,6 +4,7 @@
 #include "/usr/src/linux-2.6.11.12-hwk2/include/asm/unistd.h"
 #include <stdio.h>
 #include <errno.h>
+#include <sys/types.h>
 
 _syscall3(int, fail, int, ith,
 	 int, ncall, 
@@ -11,28 +12,96 @@ _syscall3(int, fail, int, ith,
 	 calls);
 
 int main() {
+	
 	struct syscall_failure s;
-	int res = 0;
+	int res = 0; // Result
+	
+	puts("----------------------------------------------");
+	puts("Part-1 Testing bad argument handling in fail()");
+	puts("----------------------------------------------");
+	
 	s.syscall_nr = __NR_fail;
 	s.error = ENOTSOCK; 
-	res = fail(1,1, (struct syscall_failure *) 1024);
-	if (res) perror("Error result from call 1");
-	else puts("Unexpected success on call 1");
-	res = fail(0,1,&s); 
-	if (res) perror("Unexpected failure from call 2");
-	else puts("Success on call 2");
+	
+	res = fail(-1,1, &s);
+	
+	printf("[1a] Invalid ith (-1) => fail() returned %i\n", res);
+	if (res) perror("[1a]");
+	
+	res = fail(1,-1, &s);
+	
+	printf("[1b] Invalid ncall (-1) => fail() returned %i\n", res);
+	if (res) perror("[1b]");
+	
+	res = fail(1,0, &s);
+	
+	printf("[1c] Invalid ncall (0) => fail() returned %i\n", res);
+	if (res) perror("[1c]");
+	
+	res = fail(0,1, (struct syscall_failure *) 1024);
+	
+	printf("[1d] Invalid *calls (bad pointer => fail() returned %i\n", res);
+	if (res) perror("[1d]");
+	
+	puts("------------------------------------");
+	puts("Part-2 Testing wrong usage of fail()");
+	puts("------------------------------------");
+	
 	res = fail(1,1,&s);
-	if (res) perror("Error result from call 3");
-	else puts("Unexpected success on call 3");
-	s.error = ENOMEDIUM;
+	
+	printf("[2a] A valid call to fail(1,1,__NR_fail) => returned %i\n", res);
+	if (res) perror("[2a]");
+	
 	res = fail(1,1,&s);
-	if (res) perror("Error result from call 4");
-	else puts("Success on call 4");
+	
+	printf("Same call to fail() repeated immediately => returned %i\n", res);
+	if (res) perror("[2a]");
+	
+	// This should fail because we told it to
 	res = fail(1,1,&s);
-	if (res) perror("Error result from call 5");
-	else puts("Unexpected success on call 5");
-	res = fail(1,1,&s);
-	if (res) perror("Error result from call 6");
-	else puts("Unexpected success on call 6");
+	
+	printf("Same call to fail() repeated 3rd time => returned %i\n", res);
+	if (res) perror("[2a]");
+	
+	struct syscall_failure sarr[2];
+	sarr[0] = s;
+	sarr[1] = s;
+	
+	res = fail(1,2,sarr);
+	
+	printf("[2b] Repetitive syscall_nr in *calls => fail() returned %i\n", res);
+	if (res) perror("[2b]");
+		
+	puts("-------------------------------");
+	puts("Part-3 Calling fail() in a loop");
+	puts("-------------------------------");
+	
+	puts("[3a] Calling fail() with ith = 0");
+	
+	s.syscall_nr = __NR_getpid;
+	s.error = ENOTSOCK;
+	
+	res = fail(0,1,&s);
+	if (res) perror("[3a]");
+	
+	int i;
+	pid_t pid;
+	
+	for (i = 0; i < 3; i++) {
+		pid = getpid();
+		printf("Syscall returned %i in iteration %i\n", pid, i);
+	}
+	
+	puts("[3b] Calling fail() with ith = 3");
+	
+	res = fail(3,1,&s);
+	if (res) perror("[3a]");
+	
+	for (i = 0; i < 6; i++) {
+		pid = getpid();
+		printf("Syscall returned %i in iteration %i\n", pid, i);
+	}
+	
+	//TODO Other tests
 
 }
