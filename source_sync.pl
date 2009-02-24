8fb8c158efb856b8b0839dce7931756c79eeafa8
benwarfield <benwarfield@ff732b92-f563-11dd-9d1d-173cc1d06e3c>
2009-02-24 19:52:45 +0000

    Added current manifest and known-needed subdirectories.  Also improved (I hope) source_sync.pl.

--- source_sync.pl
+++ source_sync.pl
@@ -1,14 +1,17 @@
 #!/usr/bin/perl
 
-my $SOURCE_PATH = "/usr/src/linux-2.6.11.12-hwk2";
-my $REPO_PATH = "/mnt/hgfs/kernel_homework";
+my $hwk_no = shift || 3;
+my $SOURCE_PATH = "/usr/src/linux-2.6.11.12-hwk$hwk_no";
+my $REPO_PATH = "/mnt/hgfs/kernel_homework/HW$hwk_no";
 open MANIFEST, "<$REPO_PATH/MANIFEST";
 
 for (<MANIFEST>) {
 	chomp;
 	my $sp = "$SOURCE_PATH/$_";
 	my $rp = "$REPO_PATH/$_";
-	if ( ! -e $sp or -M $sp > -M $rp) {
+	if ( ! -e $rp ) {
+		system "cp $sp $rp";
+	} elsif ( ! -e $sp or -M $sp > -M $rp) {
 		print "$_ is not up to date\n";
 #		system "diff -s $sp $rp";
 		system "cp --remove-destination $rp $sp";
