b5916f593629fc4da6ab934d074897b5028b946a
benwarfield <benwarfield@ff732b92-f563-11dd-9d1d-173cc1d06e3c>
2009-02-24 20:11:21 +0000

    Added sthreads directory and files.

---
+++ HW3/sthreads/sync.c
@@ -0,0 +1,38 @@
+/*
+ * NAME, etc.
+ *
+ * sync.c
+ *
+ * Synchronization routines for SThread
+ */
+
+#define _REENTRANT
+
+#include "sthread.h"
+
+
+/*
+ * Semaphore routines
+ */
+
+int sthread_sem_init(sthread_sem_t *sem, int count)
+{
+}
+
+int sthread_sem_destroy(sthread_sem_t *sem)
+{
+}
+
+int sthread_sem_down(sthread_sem_t *sem)
+{
+}
+
+int sthread_sem_try_down(sthread_sem_t *sem)
+{
+}
+
+int sthread_sem_up(sthread_sem_t *sem)
+{
+}
+
+
---
+++ HW3/sthreads/sync.h
@@ -0,0 +1,23 @@
+/*
+ * NAME, etc.
+ *
+ * sync.h
+ */
+
+#ifndef _STHREAD_SYNC_H_
+#define _STHREAD_SYNC_H_
+
+
+struct sthread_sem_struct {
+  /* FILL ME IN! */
+};
+
+typedef struct sthread_sem_struct sthread_sem_t;
+
+int sthread_sem_init(sthread_sem_t *sem, int count);
+int sthread_sem_destroy(sthread_sem_t *sem);
+int sthread_sem_down(sthread_sem_t *sem);
+int sthread_sem_try_down(sthread_sem_t *sem);
+int sthread_sem_up(sthread_sem_t *sem);
+
+#endif
