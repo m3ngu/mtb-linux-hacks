155c89d8fc6970baf6384a45e25ca0e7901ac6ea
thierrybm@hotmail.com <thierrybm@hotmail.com@ff732b92-f563-11dd-9d1d-173cc1d06e3c>
2009-02-25 17:59:19 +0000

    adding pseudocode to barrier

--- HW3/arch/i386/kernel/barrier.c
+++ HW3/arch/i386/kernel/barrier.c
@@ -4,3 +4,38 @@
 	Ben Warfield, Mengu Sukan, Thierry Bertin-Mahieux
 */
 
+
+/**
+ * Creates a barrier
+ */
+int barriercreate(int num)
+{
+  /*
+    create a new barrier struct
+    set the initial_count = num
+    set wait_count = 0
+    make sure queue* is null
+    add the barrier to the barrier list...?
+    set the id
+    returns the ID
+  */
+  return -666666666
+}
+
+/**
+ * Destroys a barrier
+ */
+int barrierdestroy(int barrierID)
+{
+  /*
+    find the 
+  */
+}
+
+/**
+ * Wait on the barrier, or release everyon if you're the Nth one
+ */
+int barrierwait(int barrierID);
+
+
+
