2af934d81c51b6279b335225eb7bde70819fdbd2
thierrybm@hotmail.com <thierrybm@hotmail.com@ff732b92-f563-11dd-9d1d-173cc1d06e3c>
2009-02-25 17:52:23 +0000

    tentative structure for barrier struct

--- trunk/HW3/include/linux/barrier.h
+++ trunk/HW3/include/linux/barrier.h
@@ -5,3 +5,14 @@
 	Ben Warfield, Mengu Sukan, Thierry Bertin-Mahieux
 */
 
+
+
+/**
+ * Barrier structure
+ */
+struct barrier_struct {
+  int spin_lock;             // spin lock
+  int waiting_count;         // items in queue
+  int initial_count;         // original N, or barrier size
+  void *queue; /* needs some tightening up ;-) */  // queue
+};
