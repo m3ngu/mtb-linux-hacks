a7bd84be2a062841d966af8900f4f615a9381258
thierrybm@hotmail.com <thierrybm@hotmail.com@ff732b92-f563-11dd-9d1d-173cc1d06e3c>
2009-02-27 22:57:11 +0000

    debugging

--- HW3/arch/i386/kernel/barrier.c
+++ HW3/arch/i386/kernel/barrier.c
@@ -57,7 +57,7 @@ asmlinkage int sys_barriercreate(int num)
   // init wait queue head
   //DELARE_WAIT_QUEUE_HEAD( (*b).queue );
   (*b).queue.lock = SPIN_LOCK_UNLOCKED;
-  (*b).queue.task_list = { &(*b).queue.task_list , &(*b).queue.task_list };
+  //(*b).queue.task_list = { &(*b).queue.task_list , &(*b).queue.task_list };
 
   //(*b).queue = NULL;
   // add barrier to the list
