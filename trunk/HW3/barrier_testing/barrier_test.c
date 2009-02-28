4f666daa023e5aa4670ef08f01c11dd796e0f47c
msukan <msukan@ff732b92-f563-11dd-9d1d-173cc1d06e3c>
2009-02-28 00:49:12 +0000

    Changed a few things in the test file (some things are commented out for now)

--- trunk/HW3/barrier_testing/barrier_test.c
+++ trunk/HW3/barrier_testing/barrier_test.c
@@ -5,16 +5,23 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "asm/unistd.h"
+#include <errno.h>
+#include "include/asm/unistd.h"
 
 _syscall1(int, barriercreate, 	int, num);
 _syscall1(int, barrierdestroy, 	int, id);
 _syscall1(int, barrierwait, 	int, id);
 
 int main() {
-	int barrier1 = barriercreate(1);
+	puts("[1a]: Calling barriercreare with num=0");	
+	int barrier1 = barriercreate(0);
+	perror("[1a]");
+	
+	barrier1 = barriercreate(1);
+	/*
 	fputs("Waiting for single-process barrier...", stderr);
 	if ( barrierwait(barrier1) ) perror("inexplicable failure")
 	else fputs("success!\n", stderr);
+	*/
 	return EXIT_SUCCESS;
 }
