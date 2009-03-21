11b0033e89fd6e74a9f22a6a11f65899dd35832e
msukan <msukan@ff732b92-f563-11dd-9d1d-173cc1d06e3c>
2009-03-21 20:57:42 +0000

    Added change_scheduler.c to testing

---
+++ trunk/HW4/testing/change_scheduler.c
@@ -0,0 +1,37 @@
+/*#include "linux/sched.h"  Causes a lot of warnings */
+#include <sched.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * small C program that takes a PID and changes the
+ * policy to UWRR. Should run that program as root
+ */
+
+#define DEF_PRIO	120
+#define SCHED_UWRR	4
+
+int main (int argc, char *argv[]) {
+	
+	/* Check parameter */
+	if (argc != 2) {
+		printf("Usage: %s pid\n", argv[0]);
+		return -EINVAL;
+	}
+	
+	/* Do we need to check this ? */
+	int pid = atoi(argv[1]);
+	
+	struct sched_param param = { .sched_priority = DEF_PRIO };
+	int rc;
+	
+	/* SCHED_UWRR = 4 */
+	rc = sched_setscheduler(pid, SCHED_UWRR, &param);
+	
+	if (rc) {
+		perror("Couldn't change scheduling policy");
+	}
+	
+	return rc;
+}
