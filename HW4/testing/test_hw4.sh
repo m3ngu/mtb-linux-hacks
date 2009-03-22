324dfb8051faab336a49f0da18d8f3f4a616ddc0
msukan <msukan@ff732b92-f563-11dd-9d1d-173cc1d06e3c>
2009-03-22 05:10:39 +0000

    Updated test code

--- HW4/testing/test_hw4.sh
+++ HW4/testing/test_hw4.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "Making this script run with FIFO (prio=99)"
+echo "Making this script run with FIFO (prio=50)"
 chrt -r -p 50 $$
 
 ## Changing bash prio, so that we we can kill jobs if we need to
 
-echo "Switching current bash to FIFO (prio=99)"
+#echo "Switching current bash to FIFO (prio=99)"
 bash_pid=`ps -opid,comm | grep bash | nawk '{ print $1 }'`
 chrt -f -p 99 ${bash_pid}
 
@@ -26,15 +26,15 @@ do
 	sleep 1
 	
 	echo "Submitting test_hw4 to the background as user ${u}..."
-	sudo -u ${u} -b ./inf-loop &
+	(sudo -u ${u} ./inf-loop) &
 	
-	test_pid=`ps -u ${u} -opid,comm | grep inf-loop | nawk '{ print $1 }'`
+	test_pid=`ps -u ${u} -opid,comm | grep inf-loop | tail -1 | nawk '{ print $1 }'`
 	
 	## TODO: We can't use chrt, we should have our own C program that takes
 	## a PID as argument and changes the policy to UWRR
 	echo "Switching ${test_pid} to UWRR (prio=50)"
 	##chrt -r -p 50 ${test_pid}
-	sudo ./change_scheduler ${test_pid}
+	./change-scheduler ${test_pid} 4 50
 	
 	sleep 1
 
