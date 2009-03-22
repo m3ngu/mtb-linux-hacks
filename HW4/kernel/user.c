933a4b3fbe2d90ddfc575696f4cfdac363e966f9
msukan <msukan@ff732b92-f563-11dd-9d1d-173cc1d06e3c>
2009-03-22 02:14:23 +0000

    Initalizing nr_active to 0 (2nd try)

--- HW4/kernel/user.c
+++ HW4/kernel/user.c
@@ -29,9 +29,9 @@
 		for (k = 0; k < MAX_PRIO; k++) {		\
 			INIT_LIST_HEAD(array.queue + k);		\
 			__clear_bit(k, array.bitmap);  		\
-			array.nr_active = 0;				\
 		}											\
 		__set_bit(MAX_PRIO, array.bitmap);	    	\
+		array.nr_active = 0;					\
 	}
 
 static kmem_cache_t *uid_cachep;
