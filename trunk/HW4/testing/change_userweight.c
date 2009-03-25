faa2fbdfaaddafcb3772138ca33879ebfdd1c6cd
benwarfield <benwarfield@ff732b92-f563-11dd-9d1d-173cc1d06e3c>
2009-03-25 07:30:50 +0000

    Made change-weight take username, not UID, and added it to "all" target.

--- trunk/HW4/testing/change_userweight.c
+++ trunk/HW4/testing/change_userweight.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
-#include <unistd.h>
+#include <pwd.h>
 #include "asm/unistd.h"
 
 _syscall1(int, getuserweight, int, uid)
@@ -12,8 +12,10 @@ _syscall2(int, setuserweight, int, uid, int, weight)
 
 int main (int argc, char *argv[]) {
 	if (3 != argc) exit(2);
-	int uid = atoi(argv[1]);
+	struct passwd *userpass = getpwnam(argv[1]);
 	int weight = atoi(argv[2]);
-	int ret = setuserweight(uid,weight);
+	int ret = setuserweight(userpass->pw_uid,weight);
 	if (ret) perror("Failed to set user weight");
+	ret = getuserweight(userpass->pw_uid);
+	printf("Weight for UID %d is now %d\n", userpass->pw_uid, ret);
 }
