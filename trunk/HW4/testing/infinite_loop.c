e84a06e2b09a0538ffee573773c06e20e8423add
benwarfield <benwarfield@ff732b92-f563-11dd-9d1d-173cc1d06e3c>
2009-03-25 21:52:53 +0000

    Stripped commented-out parts from infinite_loop.c

--- trunk/HW4/testing/infinite_loop.c
+++ trunk/HW4/testing/infinite_loop.c
@@ -11,31 +11,8 @@ int main (int argc, char *argv[]) {
 
 	int policy;
 	policy = sched_getscheduler(0);
-	// TODO: Now, I'm thinking that we should not set the scheduling
-	// policy here.  Scheduling policy can only be changed by root, but
-	// we want to call this program with alice, bob, carol, etc.
-	
-	// We should probably just kick this infitinite loop process off here
-	// and have another small C program that takes a PID and changes the
-	// policy to UWRR.  We can run that program as root
-	
-	/*
-	 * Change the scheduling policy of this program
-	 */
-	/*
-	struct sched_param param = { .sched_priority = 50 };
-	int rc;
-	// pid=0 current process
-	rc = sched_setscheduler(0, SCHED_RR, &param);
-	
-	
-	if (rc) {
-		perror("Couldn't change scheduling policy");
-	}
-	*/
-	
-	//TODO: infinite loop, for now this runs for a minute
 	clock_t curr_time, prev_time;
+
 	prev_time = 0;
 	unsigned short seconds_passed = 0, max_seconds = MAX_SECONDS;
 	
