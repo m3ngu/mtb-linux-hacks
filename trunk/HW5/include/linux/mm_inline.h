c876afb70a80c1b010dab34b31b67a3dd17656cb
thierrybm@hotmail.com <thierrybm@hotmail.com@ff732b92-f563-11dd-9d1d-173cc1d06e3c>
2009-04-08 23:52:16 +0000

    new add_page_to_safety_list and del_page_from_safety_list inline functions created

--- trunk/HW5/include/linux/mm_inline.h
+++ trunk/HW5/include/linux/mm_inline.h
@@ -1,4 +1,20 @@
 
+/* added HW5 */
+static inline void
+add_page_to_safety_list(struct zone *zone, struct page *page)
+{
+	list_add(&page->lru, &zone->safety_list);
+	zone->nr_safety++;
+}
+
+static inline void
+del_page_from_safety_list(struct zone *zone, struct page *page)
+{
+	list_del(&page->lru);
+	zone->nr_safety--;
+}
+/* end added HW5 */
+
 static inline void
 add_page_to_active_list(struct zone *zone, struct page *page)
 {
