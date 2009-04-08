bae2487c6d12c9401f81731d76098ab612154517
thierrybm@hotmail.com <thierrybm@hotmail.com@ff732b92-f563-11dd-9d1d-173cc1d06e3c>
2009-04-08 22:21:55 +0000

    stupid header that contains CACHE_NORMAL and CACHE_MRU

---
+++ trunk/HW5/include/linux/hw5_definitions.h
@@ -0,0 +1,18 @@
+/**
+ * T. Bertin-Mahieux, M. Sukan, B. Warfield
+ * TEAM 2 - OS Spring 2009
+ * Simple file added for HW5, containt two int definitions
+ * WOW!!!!!!!!!!!!!
+ */
+
+
+
+#ifndef _LINUX_HW5_DEFINITIONS_H
+#define _LINUX_HW5_DEFINITIONS_H
+
+
+#define CACHE_NORMAL 392029
+#define CACHE_MRU 6638229
+
+
+#endif // _LINUX_HW5_DEFINITIONS_H
