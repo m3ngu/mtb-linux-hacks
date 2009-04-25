575c05ef908365bb5990693e8eccc53844975a2a
benwarfield <benwarfield@ff732b92-f563-11dd-9d1d-173cc1d06e3c>
2009-04-25 21:23:54 +0000

    Added tag functions to directory and plain file operation tables.

--- trunk/HW6/fs/ext2/file.c
+++ trunk/HW6/fs/ext2/file.c
@@ -65,4 +65,8 @@ struct inode_operations ext2_file_inode_operations = {
 #endif
 	.setattr	= ext2_setattr,
 	.permission	= ext2_permission,
+	/* HW6 additions: */
+	.add_tag 	= ext2_addtag,
+	.rm_tag 	= ext2_rmtag,
+	.get_tags	= ext2_gettags,	
 };
